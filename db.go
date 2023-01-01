9ce6ce90110d4f5f489e22980f9056ff05d32016
Leonard Velásquez <leonard.avp@gmail.com>
2023-01-01 18:56:03 -0400

    Crud de empresas y archivos base

---
+++ db.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func GetDBInstance() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("calendario-fiscal.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func InitDB() {
+	GetDBInstance().AutoMigrate()
+}
